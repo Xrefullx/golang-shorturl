8be3ade5391b8ba7e0f62ac9ef914fd68e2a0ca5
Xrefullx <79327529+Xrefullx@users.noreply.github.com>
2022-12-20 08:27:03 +0700

    Update router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,6 +9,5 @@ func CreateRouter(handler handlers.Handler) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/{shortID}", handler.GetHandler)
 	r.Post("/", handler.SaveHandler)
-	r.Post("/api/shorten", handler.JsonSave)
 	return r
 }
